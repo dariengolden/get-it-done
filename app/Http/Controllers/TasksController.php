ce4838cdd370605afda82a15e322ad474179f11c
dariengolden <darienngoldenn@gmail.com>
2024-10-25 01:11:00 +0700

    MySQL migration file, TasksController, and Task model created using 'php artisan'.

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function index() {
+        return view('tasks.index');
+    }
+
+    public function create() {
+        return view('tasks.create');
+    }
+
+    public function store() {
+        $task = Task::create(
+          [
+            'description' => request('description')
+            ]
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TasksController;
+
+Route::get('/user/{id}', [UserController::class, 'show']);
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('tasks.index');
-});
+Route::get('/', [TasksController::class, 'index']);
+Route::get('/tasks', [TasksController::class, 'index']);
+Route::get('/tasks/create', [TasksController::class, 'create']);
 
-Route::get('/tasks/create', function() {
-    return view('tasks.create');
-});
+Route::post('/tasks', [TasksController::class, 'store']);
